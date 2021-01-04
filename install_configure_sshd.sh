b0107441781e785ffd21831e3eae53fcf22328a7
Vladimir Badai <vladimir.baday@gmail.com>
2021-01-04 18:29:22 +0200

    fix sed in sshd_config

--- install_configure_sshd.sh
+++ install_configure_sshd.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 
-if grep -q '#AuthorizedKeysCommand none' "$SSHD_CONFIG_FILE"; then
-  sed -i "s:#AuthorizedKeysCommand none:AuthorizedKeysCommand ${AUTHORIZED_KEYS_COMMAND_FILE}:g" "$SSHD_CONFIG_FILE"
+if grep -q 'AuthorizedKeysCommand ' "$SSHD_CONFIG_FILE"; then
+  sed -i "s:.*AuthorizedKeysCommand\s.*:AuthorizedKeysCommand ${AUTHORIZED_KEYS_COMMAND_FILE}:g" "$SSHD_CONFIG_FILE"
 else
   if ! grep -q "AuthorizedKeysCommand ${AUTHORIZED_KEYS_COMMAND_FILE}" "$SSHD_CONFIG_FILE"; then
     echo "AuthorizedKeysCommand ${AUTHORIZED_KEYS_COMMAND_FILE}" >> "$SSHD_CONFIG_FILE"
   fi
 fi
 
-if grep -q '#AuthorizedKeysCommandUser nobody' "$SSHD_CONFIG_FILE"; then
-  sed -i "s:#AuthorizedKeysCommandUser nobody:AuthorizedKeysCommandUser nobody:g" "$SSHD_CONFIG_FILE"
+if grep -q 'AuthorizedKeysCommandUser ' "$SSHD_CONFIG_FILE"; then
+  sed -i "s:.*AuthorizedKeysCommandUser\s.*:AuthorizedKeysCommandUser nobody:g" "$SSHD_CONFIG_FILE"
 else
   if ! grep -q 'AuthorizedKeysCommandUser nobody' "$SSHD_CONFIG_FILE"; then
     echo "AuthorizedKeysCommandUser nobody" >> "$SSHD_CONFIG_FILE"
